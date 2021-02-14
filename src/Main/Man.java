f7c93157ac31dcba87aa19807153becaf6a5b670
Kirill Reuka <reuka.kirill@gmail.com>
2021-02-14 14:18:01 +0200

    Done

--- src/Main/Man.java
+++ src/Main/Man.java
@@ -1,5 +1,26 @@
-package Man;
+package Main;
 
-public class Man {
+public class Man extends Human {
+    private String beardColor;
 
+    public Man(String name, String surname, int height, int weight, String  beardColor) {
+        super(name, surname, height, weight);
+        this.beardColor = beardColor;
+    }
+
+    public String getBeardColor() {
+        return beardColor;
+    }
+
+    public void setBeardColor(String beardColor) {
+        this.beardColor = beardColor;
+    }
+
+    public void displayManInfo() {
+        System.out.println("Name - " + getName());
+        System.out.println("Surname - " + getSurname());
+        System.out.println("Height - " + getHeight());
+        System.out.println("Weight - " + getWeight());
+        System.out.println("Beard color - " + getBeardColor());
+    }
 }
